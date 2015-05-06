e813be9f300d40335f424409e9c067ddafa7dc75
Vilhelm von Ehrenheim <vilhelm.ehrenheim@klarna.com>
2015-05-06 08:29:25 +0200

    Finnished incomplete gamma function and added some tests.

--- src/gamma.erl
+++ src/gamma.erl
@@ -9,6 +9,8 @@
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
+-define(FPMIN, 1.00208418000449e-292).
+-define(EPS, 2.22044604925031e-16).
 
 % ------------------------------------------------------------------------------
 %  gampdf - Uniform probability density function
@@ -65,25 +67,11 @@ gammaln(X) ->
         Tmp2+math:log(2.5066282746310005*Ser/X).
 
 %% Incomplete Gamma function
-gammap(A,X) when X =< 0 orelse A =< 0 -> {error, "Bad args in gammap."};
+gammap(A,X) when X < 0 orelse A =< 0 -> {error, "Bad args in gammap."};
 gammap(_,0.0) -> 0.0;
 gammap(A,X) when A >= 100 -> gammapapprox(A,X,1);
 gammap(A,X) when X =< A + 1.0 -> gser(A,X);
 gammap(A,X) -> 1.0 - gcf(A,X).
-
-
-%% Series
-gser(A,X) -> gser_(A, X, A, 1.0/A, 1.0/A).
-
-%% Tail recusrion for gser
-gser_(A,X,_,Del,Sum) when abs(Del) < abs(Sum)*1.0e-16 -> 
-        Sum*math:exp(-X+A*math:log(X)-gammaln(A));
-gser_(A,X,Ap,Del,Sum) ->
-        gser_(A, X, Ap+1, Del*X/Ap, Sum+Del).
-
-
-%%
-gcf(_,_) -> not_implemented.
 
 
 %% Abscissas for Gauss-Legendre quadrature
@@ -102,7 +90,7 @@ gauleg18_w() ->
           0.047235083490265582,  0.053244713977759692, 0.058860144245324798,
           0.064039797355015485,  0.068745323835736408, 0.072941885005653087,
           0.076598410645870640,  0.079687828912071670, 0.082187266704339706,
-          0.084078218979661945,  0.085346685739338721,0.085983275670394821 ].
+          0.084078218979661945,  0.085346685739338721, 0.085983275670394821 ].
 
 %% Approximate incomplete gamma function by Gauss-Legendre quadrature
 gammapapprox(A,X,Psig) ->
@@ -126,6 +114,40 @@ gammapapprox(A,X,Psig) ->
         end.
 
 
+%% Approximate incomplete gamma function by seriies expansion
+gser(A,X) -> gser_(A, X, A, 1.0/A, 1.0/A).
+
+%% Tail recusrion for gser
+gser_(A,X,_,Del,Sum) when abs(Del) < abs(Sum)*?EPS -> 
+        Sum*math:exp(-X+A*math:log(X)-gammaln(A));
+gser_(A,X,Ap,Del,Sum) ->
+        Ap2 = Ap + 1,
+        Del2 = Del*X/Ap2,
+        Sum2 = Sum+Del2,
+        gser_(A, X, Ap2, Del2, Sum2).
+
+
+%%
+gcf(A,X) -> 
+        B = X + 1.0 - A,
+        D = 1.0/B,
+        gcf_(A,X,B,1.0/?FPMIN,D,D,1).
+
+gcf_(A,X,_,C,D,H,_) when abs(D*C-1) =< ?EPS ->
+        math:exp(-X+A*math:log(X)-gammaln(A))*H;
+
+gcf_(A,X,B,C,D,H,I) ->
+        An = -I*(I-A),
+        B2 = B + 2,
+        D2 = if abs(An*D+B2) < ?FPMIN -> 1.0/?FPMIN
+              ; abs(An*D+B2) >=?FPMIN -> 1.0/(An*D+B2)
+             end,
+        C2 = if abs(B2+An/C) < ?FPMIN -> ?FPMIN
+              ; abs(B2+An/C) >=?FPMIN -> B2+An/C
+             end,
+        gcf_(A,X,B2,C2,D2,H*D2*C2,I+1).
+
+
 invgammap(_,_) -> not_implemented.
 
 % ==============================================================================
@@ -138,15 +160,24 @@ gammaln_test() ->
         ?assertEqual(-0.09447840768115956, gammaln(1.234)),
         ?assertEqual(18.734347511936445, gammaln(12.5)).
 
+gammap_test() ->
+        ?assertEqual({error, "Bad args in gammap."}, gammap(-1.0, 1.0)),
+        ?assertEqual({error, "Bad args in gammap."}, gammap( 1.0,-1.0)),
+        ?assertEqual(0.0, gammap(1.0, 0.0)).
+
 gampdf_test() ->
         ?assertEqual(0.0, gampdf(0.0,2,2)),
         ?assertEqual(0.2651846416468157, gampdf(2.3,3,1)),
+        ?assertEqual(1.9199765904692206e-04, gampdf(20,20,2)),
         ?assertEqual(0.5413411329464507, gampdf(1.0,2,2)).
 
 gamcdf_test() ->
         ?assertEqual(0.0, gamcdf(0.0,1,1)),
         ?assertEqual(0.5132987982791589, gamcdf(100,100,1)),
-        ?assertEqual(0.124781215032525, gamcdf(15,20,1)).
+        ?assertEqual(0.5297427331607533, gamcdf(20,20,1)),
+        ?assertEqual(0.9998236971022615, gamcdf(20,20,2)),
+        ?assertEqual(0.12478121503252411, gamcdf(15,20,1)),
+        ?assertEqual(0.9595723180054873, gamcdf(5,2,1)).
 
 %gaminv_test() ->
 %        ?assertEqual(0.0, gaminv(0.0,1,1)).
