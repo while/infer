ca2cd4fa05fb78a42d9c29fab213e4c83e20ca73
Vilhelm von Ehrenheim <vilhelm.ehrenheim@klarna.com>
2016-04-01 11:37:20 +0200

    Renamed distributions functions in chisq.

--- src/chisq.erl
+++ src/chisq.erl
@@ -3,41 +3,41 @@
 % ==============================================================================
 -module(chisq).
 
--export([chisqpdf/2, chisqcdf/2, chisqinv/2]).
+-export([pdf/2, cdf/2, invcdf/2]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
 % ------------------------------------------------------------------------------
-%  chisqpdf - Uniform probability density function
+%  pdf - Uniform probability density function
 % ------------------------------------------------------------------------------
-chisqpdf(_,Nu) when Nu =< 0 -> {error,"Nu parameter =< 0 in Chi-square."};
+pdf(_,Nu) when Nu =< 0 -> {error,"Nu parameter =< 0 in Chi-square."};
 
-chisqpdf(X,_) when X < 0 -> 0.0;
-chisqpdf(X,Nu) ->
+pdf(X,_) when X < 0 -> 0.0;
+pdf(X,Nu) ->
         Fac = 0.693147180559945309*(0.5*Nu) + incgammabeta:gammaln(0.5*Nu),
         math:exp(-0.5*(X-(Nu-2)*math:log(X))-Fac).
 
 % ------------------------------------------------------------------------------
-%  chisqcdf - Uniform cumulative distribution function
+%  cdf - Uniform cumulative distribution function
 % ------------------------------------------------------------------------------
-chisqcdf(_,Nu) when Nu =< 0 -> {error,"Nu parameter =< 0 in Chi-square."};
+cdf(_,Nu) when Nu =< 0 -> {error,"Nu parameter =< 0 in Chi-square."};
 
-chisqcdf(X,_) when X < 0 -> 0.0;
-chisqcdf(X,Nu) ->
+cdf(X,_) when X < 0 -> 0.0;
+cdf(X,Nu) ->
         incgammabeta:gammap(0.5*Nu,0.5*X).
 
         
 % ------------------------------------------------------------------------------
-%  chisqinv - Inverse chisq distribution function
+%  invcdf - Inverse chisq distribution function
 % ------------------------------------------------------------------------------
-chisqinv(_,Nu) when Nu =< 0 -> {error,"Nu parameter =< 0 in Chi-square."};
+invcdf(_,Nu) when Nu =< 0 -> {error,"Nu parameter =< 0 in Chi-square."};
 
-chisqinv(P,_) when P < 0 orelse P > 1 -> {error,"Invalid probability"};
+invcdf(P,_) when P < 0 orelse P > 1 -> {error,"Invalid probability"};
 
-chisqinv(X,_) when X =< 0 -> 0.0;
-chisqinv(P,Nu) ->
+invcdf(X,_) when X =< 0 -> 0.0;
+invcdf(P,Nu) ->
         2.0*incgammabeta:invgammap(P,0.5*Nu).
 
 
@@ -46,41 +46,41 @@ chisqinv(P,Nu) ->
 % ------------------------------------------------------------------------------
 -ifdef(TEST).
 
-chisqpdf_test() ->
-        ?assertEqual(0.0, chisqpdf(-1.0,1)),
-        ?assertEqual(0.24197072451914325, chisqpdf(1,1)),
-        ?assertEqual(0.10377687435514864, chisqpdf(2,1)),
-        ?assertEqual(0.3032653298563167, chisqpdf(1,2)),
-        ?assertEqual(0.003368973499542733, chisqpdf(10,2)),
-        ?assertEqual(0.00766415502440505, chisqpdf(2,10)).
+pdf_test() ->
+        ?assertEqual(0.0, pdf(-1.0,1)),
+        ?assertEqual(0.24197072451914325, pdf(1,1)),
+        ?assertEqual(0.10377687435514864, pdf(2,1)),
+        ?assertEqual(0.3032653298563167, pdf(1,2)),
+        ?assertEqual(0.003368973499542733, pdf(10,2)),
+        ?assertEqual(0.00766415502440505, pdf(2,10)).
 
-chisqpdf_error_test() ->
-        ?assertEqual({error,"Nu parameter =< 0 in Chi-square."},  chisqpdf(0.0,-1)).
+pdf_error_test() ->
+        ?assertEqual({error,"Nu parameter =< 0 in Chi-square."},  pdf(0.0,-1)).
 
-chisqcdf_test() ->
-        ?assertEqual(0.0, chisqcdf(-1.0,1)),
-        ?assertEqual(0.6826894921370856, chisqcdf(1,1)),
-        ?assertEqual(0.8427007929497147, chisqcdf(2,1)),
-        ?assertEqual(0.3934693402873665, chisqcdf(1,2)),
-        ?assertEqual(0.9932620530009145, chisqcdf(10,2)),
-        ?assertEqual(0.0036598468273437135, chisqcdf(2,10)),
-        ?assertEqual(0.5132987982791589, chisqcdf(200,200)).
+cdf_test() ->
+        ?assertEqual(0.0, cdf(-1.0,1)),
+        ?assertEqual(0.6826894921370856, cdf(1,1)),
+        ?assertEqual(0.8427007929497147, cdf(2,1)),
+        ?assertEqual(0.3934693402873665, cdf(1,2)),
+        ?assertEqual(0.9932620530009145, cdf(10,2)),
+        ?assertEqual(0.0036598468273437135, cdf(2,10)),
+        ?assertEqual(0.5132987982791589, cdf(200,200)).
 
-chisqcdf_error_test() ->
-        ?assertEqual({error,"Nu parameter =< 0 in Chi-square."},  chisqcdf(0.0,-1)).
+cdf_error_test() ->
+        ?assertEqual({error,"Nu parameter =< 0 in Chi-square."},  cdf(0.0,-1)).
 
-chisqinv_test() ->
-        ?assertEqual(0.0, chisqinv(0.0,1)),
-        ?assertEqual(0.015790774093431218, chisqinv(0.1,1)),
-        ?assertEqual(0.2107210313156527, chisqinv(0.1,2)),
-        ?assertEqual(0.45493642311957305, chisqinv(0.5,1)),
-        ?assertEqual(1.3862943611198906, chisqinv(0.5,2)),
-        ?assertEqual(15.987179172105261, chisqinv(0.9,10)).
+invcdf_test() ->
+        ?assertEqual(0.0, invcdf(0.0,1)),
+        ?assertEqual(0.015790774093431218, invcdf(0.1,1)),
+        ?assertEqual(0.2107210313156527, invcdf(0.1,2)),
+        ?assertEqual(0.45493642311957305, invcdf(0.5,1)),
+        ?assertEqual(1.3862943611198906, invcdf(0.5,2)),
+        ?assertEqual(15.987179172105261, invcdf(0.9,10)).
 
-chisqinv_error_test() ->
-        ?assertEqual({error,"Nu parameter =< 0 in Chi-square."},  chisqinv(0.5,-1)),
-        ?assertEqual({error,"Invalid probability"},  chisqinv(-0.5,1)),
-        ?assertEqual({error,"Invalid probability"},  chisqinv(2,1)).
+invcdf_error_test() ->
+        ?assertEqual({error,"Nu parameter =< 0 in Chi-square."},  invcdf(0.5,-1)),
+        ?assertEqual({error,"Invalid probability"},  invcdf(-0.5,1)),
+        ?assertEqual({error,"Invalid probability"},  invcdf(2,1)).
 
 -endif.
 
