5bee744c28b47c2ac243d75554ff658bafc68446
Vilhelm von Ehrenheim <vilhelm.ehrenheim@klarna.com>
2016-04-01 11:40:48 +0200

    Renamed distributions functions in fdist.

--- src/fdist.erl
+++ src/fdist.erl
@@ -3,20 +3,20 @@
 % ==============================================================================
 -module(fdist).
 
--export([fdistpdf/3, fdistcdf/3, fdistinv/3]).
+-export([pdf/3, cdf/3, invcdf/3]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
 % ------------------------------------------------------------------------------
-%  fdistpdf - Probability density function
+%  pdf - Probability density function
 % ------------------------------------------------------------------------------
-fdistpdf(_,Nu1,_) when Nu1 =< 0 -> {error,"Nu1 parameter =< 0 in F-dist."};
-fdistpdf(_,_,Nu2) when Nu2 =< 0 -> {error,"Nu2 parameter =< 0 in F-dist."};
+pdf(_,Nu1,_) when Nu1 =< 0 -> {error,"Nu1 parameter =< 0 in F-dist."};
+pdf(_,_,Nu2) when Nu2 =< 0 -> {error,"Nu2 parameter =< 0 in F-dist."};
 
-fdistpdf(X,_,_) when X < 0 -> 0.0;
-fdistpdf(X,Nu1,Nu2) ->
+pdf(X,_,_) when X < 0 -> 0.0;
+pdf(X,Nu1,Nu2) ->
         Fac = 0.5*(Nu1*math:log(Nu1) + Nu2*math:log(Nu2))
               + incgammabeta:gammaln(0.5*(Nu1+Nu2))
               - incgammabeta:gammaln(0.5*Nu1)
@@ -24,26 +24,26 @@ fdistpdf(X,Nu1,Nu2) ->
         math:exp((0.5*Nu1-1.0)*math:log(X)-0.5*(Nu1+Nu2)*math:log(Nu2+Nu1*X)+Fac).
 
 % ------------------------------------------------------------------------------
-%  fdistcdf - Cumulative distribution function
+%  cdf - Cumulative distribution function
 % ------------------------------------------------------------------------------
-fdistcdf(_,Nu1,_) when Nu1 =< 0 -> {error,"Nu1 parameter =< 0 in F-dist."};
-fdistcdf(_,_,Nu2) when Nu2 =< 0 -> {error,"Nu2 parameter =< 0 in F-dist."};
+cdf(_,Nu1,_) when Nu1 =< 0 -> {error,"Nu1 parameter =< 0 in F-dist."};
+cdf(_,_,Nu2) when Nu2 =< 0 -> {error,"Nu2 parameter =< 0 in F-dist."};
 
-fdistcdf(X,_,_) when X < 0 -> 0.0;
-fdistcdf(X,Nu1,Nu2) ->
+cdf(X,_,_) when X < 0 -> 0.0;
+cdf(X,Nu1,Nu2) ->
         incgammabeta:betai(0.5*Nu1,0.5*Nu2,Nu1*X/(Nu2+Nu1*X)).
 
         
 % ------------------------------------------------------------------------------
-%  fdistinv - Inverse cumulative distribution function
+%  invcdf - Inverse cumulative distribution function
 % ------------------------------------------------------------------------------
-fdistinv(_,Nu1,_) when Nu1 =< 0 -> {error,"Nu1 parameter =< 0 in F-dist."};
-fdistinv(_,_,Nu2) when Nu2 =< 0 -> {error,"Nu2 parameter =< 0 in F-dist."};
+invcdf(_,Nu1,_) when Nu1 =< 0 -> {error,"Nu1 parameter =< 0 in F-dist."};
+invcdf(_,_,Nu2) when Nu2 =< 0 -> {error,"Nu2 parameter =< 0 in F-dist."};
 
-fdistinv(P,_,_) when P < 0 orelse P > 1 -> {error,"Invalid probability"};
+invcdf(P,_,_) when P < 0 orelse P > 1 -> {error,"Invalid probability"};
 
-fdistinv(X,_,_) when X =< 0 -> 0.0;
-fdistinv(P,Nu1,Nu2) ->
+invcdf(X,_,_) when X =< 0 -> 0.0;
+invcdf(P,Nu1,Nu2) ->
         InvB = incgammabeta:invbetai(P,0.5*Nu1,0.5*Nu2),
         Nu2*InvB/(Nu1*(1.0-InvB)).
 
@@ -54,45 +54,45 @@ fdistinv(P,Nu1,Nu2) ->
 % ------------------------------------------------------------------------------
 -ifdef(TEST).
 
-fdistpdf_test() ->
-        ?assertEqual(0.0, fdistpdf(-1.0,1,1)),
-        ?assertEqual(0.1591549430918952, fdistpdf(1,1,1)),
-        ?assertEqual(0.07502635967975876, fdistpdf(2,1,1)),
-        ?assertEqual(0.19245008972987535, fdistpdf(1,2,1)),
-        ?assertEqual(0.19245008972987535, fdistpdf(1,1,2)),
-        ?assertEqual(0.010391328106475825, fdistpdf(10,2,1)),
-        ?assertEqual(0.00614445752989748, fdistpdf(10,2,3)),
-        ?assertEqual(0.10519242090293805, fdistpdf(2,10,1)),
-        ?assertEqual(0.0027551055016847756, fdistpdf(2,100,100)).
+pdf_test() ->
+        ?assertEqual(0.0, pdf(-1.0,1,1)),
+        ?assertEqual(0.1591549430918952, pdf(1,1,1)),
+        ?assertEqual(0.07502635967975876, pdf(2,1,1)),
+        ?assertEqual(0.19245008972987535, pdf(1,2,1)),
+        ?assertEqual(0.19245008972987535, pdf(1,1,2)),
+        ?assertEqual(0.010391328106475825, pdf(10,2,1)),
+        ?assertEqual(0.00614445752989748, pdf(10,2,3)),
+        ?assertEqual(0.10519242090293805, pdf(2,10,1)),
+        ?assertEqual(0.0027551055016847756, pdf(2,100,100)).
 
-fdistpdf_error_test() ->
-        ?assertEqual({error,"Nu1 parameter =< 0 in F-dist."},  fdistpdf(0.0,-1,1)),
-        ?assertEqual({error,"Nu2 parameter =< 0 in F-dist."},  fdistpdf(0.0,1,-1)).
+pdf_error_test() ->
+        ?assertEqual({error,"Nu1 parameter =< 0 in F-dist."},  pdf(0.0,-1,1)),
+        ?assertEqual({error,"Nu2 parameter =< 0 in F-dist."},  pdf(0.0,1,-1)).
 
-fdistcdf_test() ->
-        ?assertEqual(0.0, fdistcdf(-1.0,1,1)),
-        ?assertEqual(0.5000000000000007, fdistcdf(1,1,1)),
-        ?assertEqual(0.6081734479693931, fdistcdf(2,1,1)),
-        ?assertEqual(0.42264973081037405, fdistcdf(1,2,1)),
-        ?assertEqual(0.7817821097640074, fdistcdf(10,2,1)),
-        ?assertEqual(0.4956475043831201, fdistcdf(2,10,1)).
+cdf_test() ->
+        ?assertEqual(0.0, cdf(-1.0,1,1)),
+        ?assertEqual(0.5000000000000007, cdf(1,1,1)),
+        ?assertEqual(0.6081734479693931, cdf(2,1,1)),
+        ?assertEqual(0.42264973081037405, cdf(1,2,1)),
+        ?assertEqual(0.7817821097640074, cdf(10,2,1)),
+        ?assertEqual(0.4956475043831201, cdf(2,10,1)).
 
-fdistcdf_error_test() ->
-        ?assertEqual({error,"Nu1 parameter =< 0 in F-dist."},  fdistcdf(0.0,-1,1)),
-        ?assertEqual({error,"Nu2 parameter =< 0 in F-dist."},  fdistcdf(0.0,1,-1)).
+cdf_error_test() ->
+        ?assertEqual({error,"Nu1 parameter =< 0 in F-dist."},  cdf(0.0,-1,1)),
+        ?assertEqual({error,"Nu2 parameter =< 0 in F-dist."},  cdf(0.0,1,-1)).
 
-fdistinv_test() ->
-        ?assertEqual(0.0, fdistinv(0.0,1,1)),
-        ?assertEqual(0.025085630936916625, fdistinv(0.1,1,1)),
-        ?assertEqual(0.1172839506172838, fdistinv(0.1,2,1)),
-        ?assertEqual(0.9999999999999966, fdistinv(0.5,1,1)),
-        ?assertEqual(1.5000000000000018, fdistinv(0.5,2,1)),
-        ?assertEqual(60.19498034404559, fdistinv(0.9,10,1)).
+invcdf_test() ->
+        ?assertEqual(0.0, invcdf(0.0,1,1)),
+        ?assertEqual(0.025085630936916625, invcdf(0.1,1,1)),
+        ?assertEqual(0.1172839506172838, invcdf(0.1,2,1)),
+        ?assertEqual(0.9999999999999966, invcdf(0.5,1,1)),
+        ?assertEqual(1.5000000000000018, invcdf(0.5,2,1)),
+        ?assertEqual(60.19498034404559, invcdf(0.9,10,1)).
 
-fdistinv_error_test() ->
-        ?assertEqual({error,"Nu1 parameter =< 0 in F-dist."},  fdistinv(0.0,-1,1)),
-        ?assertEqual({error,"Nu2 parameter =< 0 in F-dist."},  fdistinv(0.0,1,-1)),
-        ?assertEqual({error,"Invalid probability"},  fdistinv(-0.1,1,1)).
+invcdf_error_test() ->
+        ?assertEqual({error,"Nu1 parameter =< 0 in F-dist."},  invcdf(0.0,-1,1)),
+        ?assertEqual({error,"Nu2 parameter =< 0 in F-dist."},  invcdf(0.0,1,-1)),
+        ?assertEqual({error,"Invalid probability"},  invcdf(-0.1,1,1)).
 
 -endif.
 
