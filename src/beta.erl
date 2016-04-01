5a16faabded6b187a47994b562341ec78349a629
Vilhelm von Ehrenheim <vilhelm.ehrenheim@klarna.com>
2016-04-01 11:34:44 +0200

    Renamed distributions functions in beta.

--- src/beta.erl
+++ src/beta.erl
@@ -3,7 +3,7 @@
 % ==============================================================================
 -module(beta).
 
--export([betapdf/3, betacdf/3, betainv/3]).
+-export([pdf/3, cdf/3, invcdf/3]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -12,45 +12,45 @@
 -define(SQR(X), ((X)*(X))).
 
 % ------------------------------------------------------------------------------
-%  betapdf - Probability density function
+%  pdf - Probability density function
 % ------------------------------------------------------------------------------
-betapdf(_,Alpha,_) when Alpha =< 0 -> 
-        {error,"Alpha parameter =< 0 in betapdf."};
-betapdf(_,_,Beta) when Beta =< 0 -> 
-        {error,"Beta parameter =< 0 in betapdf."};
-betapdf(X,_,_) when X =< 0.0 orelse X >= 1.0 -> 
-        {error,"Bad X parameter in betapdf."};
+pdf(_,Alpha,_) when Alpha =< 0 -> 
+        {error,"Alpha parameter =< 0 in pdf."};
+pdf(_,_,Beta) when Beta =< 0 -> 
+        {error,"Beta parameter =< 0 in pdf."};
+pdf(X,_,_) when X =< 0.0 orelse X >= 1.0 -> 
+        {error,"Bad X parameter in pdf."};
 
-betapdf(X,Alpha,Beta) ->
+pdf(X,Alpha,Beta) ->
         Fac = incgammabeta:gammaln(Alpha+Beta) - incgammabeta:gammaln(Alpha)
               - incgammabeta:gammaln(Beta),
         math:exp((Alpha-1.0)*math:log(X)+(Beta-1.0)*math:log(1.0-X)+Fac).
 
 % ------------------------------------------------------------------------------
-%  betacdf - Cumulative distribution function
+%  cdf - Cumulative distribution function
 % ------------------------------------------------------------------------------
-betacdf(_,Alpha,_) when Alpha =< 0 -> 
-        {error,"Alpha parameter =< 0 in betacdf."};
-betacdf(_,_,Beta) when Beta =< 0 -> 
-        {error,"Beta parameter =< 0 in betacdf."};
-betacdf(X,_,_) when X =< 0.0 orelse X >= 1.0 -> 
-        {error,"Bad X parameter in betacdf."};
+cdf(_,Alpha,_) when Alpha =< 0 -> 
+        {error,"Alpha parameter =< 0 in cdf."};
+cdf(_,_,Beta) when Beta =< 0 -> 
+        {error,"Beta parameter =< 0 in cdf."};
+cdf(X,_,_) when X =< 0.0 orelse X >= 1.0 -> 
+        {error,"Bad X parameter in cdf."};
 
-betacdf(X,Alpha,Beta) ->
+cdf(X,Alpha,Beta) ->
         incgammabeta:betai(Alpha,Beta,X).
 
         
 % ------------------------------------------------------------------------------
-%  betainv - Inverse cumulative distribution function
+%  invcdf - Inverse cumulative distribution function
 % ------------------------------------------------------------------------------
-betainv(_,Alpha,_) when Alpha =< 0 -> 
-        {error,"Alpha parameter =< 0 in betainv."};
-betainv(_,_,Beta) when Beta =< 0 -> 
-        {error,"Beta parameter =< 0 in betainv."};
+invcdf(_,Alpha,_) when Alpha =< 0 -> 
+        {error,"Alpha parameter =< 0 in invcdf."};
+invcdf(_,_,Beta) when Beta =< 0 -> 
+        {error,"Beta parameter =< 0 in invcdf."};
 
-betainv(P,_,_) when P < 0 orelse P > 1 -> {error,"Invalid probability"};
+invcdf(P,_,_) when P < 0 orelse P > 1 -> {error,"Invalid probability"};
 
-betainv(P,Alpha,Beta) -> 
+invcdf(P,Alpha,Beta) -> 
         incgammabeta:invbetai(P,Alpha,Beta).
 
 
@@ -61,41 +61,41 @@ betainv(P,Alpha,Beta) ->
 % ------------------------------------------------------------------------------
 -ifdef(TEST).
 
-betapdf_test() ->
-        ?assertEqual(3.2804999999999933, betapdf(0.1,1,5)),
-        ?assertEqual(3.8742048900000183, betapdf(0.1,1,10)),
-        ?assertEqual(1.8749999999999938, betapdf(0.5,3,3)),
-        ?assertEqual(1.0610329539459684, betapdf(0.9,0.5,0.5)).
+pdf_test() ->
+        ?assertEqual(3.2804999999999933, pdf(0.1,1,5)),
+        ?assertEqual(3.8742048900000183, pdf(0.1,1,10)),
+        ?assertEqual(1.8749999999999938, pdf(0.5,3,3)),
+        ?assertEqual(1.0610329539459684, pdf(0.9,0.5,0.5)).
 
-betapdf_error_test() ->
-        ?assertEqual({error,"Alpha parameter =< 0 in betapdf."},  betapdf(0.0,-1,1)),
-        ?assertEqual({error,"Beta parameter =< 0 in betapdf."},  betapdf(0.0,1,-1)),
-        ?assertEqual({error,"Bad X parameter in betapdf."},  betapdf(-1.0,1,1)),
-        ?assertEqual({error,"Bad X parameter in betapdf."},  betapdf(2.0,1,1)).
+pdf_error_test() ->
+        ?assertEqual({error,"Alpha parameter =< 0 in pdf."},  pdf(0.0,-1,1)),
+        ?assertEqual({error,"Beta parameter =< 0 in pdf."},  pdf(0.0,1,-1)),
+        ?assertEqual({error,"Bad X parameter in pdf."},  pdf(-1.0,1,1)),
+        ?assertEqual({error,"Bad X parameter in pdf."},  pdf(2.0,1,1)).
 
-betacdf_test() ->
-        ?assertEqual(0.4095099999999993, betacdf(0.1,1,5)),
-        ?assertEqual(0.6513215599000027, betacdf(0.1,1,10)),
-        ?assertEqual(0.5000000000000016, betacdf(0.5,3,3)),
-        ?assertEqual(0.7951672353008667, betacdf(0.9,0.5,0.5)).
+cdf_test() ->
+        ?assertEqual(0.4095099999999993, cdf(0.1,1,5)),
+        ?assertEqual(0.6513215599000027, cdf(0.1,1,10)),
+        ?assertEqual(0.5000000000000016, cdf(0.5,3,3)),
+        ?assertEqual(0.7951672353008667, cdf(0.9,0.5,0.5)).
 
-betacdf_param_test() ->
-        ?assertEqual({error,"Alpha parameter =< 0 in betacdf."},  betacdf(0.0,-1,1)),
-        ?assertEqual({error,"Beta parameter =< 0 in betacdf."},  betacdf(0.0,1,-1)),
-        ?assertEqual({error,"Bad X parameter in betacdf."},  betacdf(-1.0,1,1)),
-        ?assertEqual({error,"Bad X parameter in betacdf."},  betacdf(2.0,1,1)).
+cdf_param_test() ->
+        ?assertEqual({error,"Alpha parameter =< 0 in cdf."},  cdf(0.0,-1,1)),
+        ?assertEqual({error,"Beta parameter =< 0 in cdf."},  cdf(0.0,1,-1)),
+        ?assertEqual({error,"Bad X parameter in cdf."},  cdf(-1.0,1,1)),
+        ?assertEqual({error,"Bad X parameter in cdf."},  cdf(2.0,1,1)).
 
-betainv_test() ->
-        ?assertEqual(0.020851637639023268, betainv(0.1,1,5)),
-        ?assertEqual(0.010480741793785553, betainv(0.1,1,10)),
-        ?assertEqual(0.499999999999999, betainv(0.5,3,3)),
-        ?assertEqual(0.610181649469041, betainv(0.7,3,3)),
-        ?assertEqual(0.9755282581475768, betainv(0.9,0.5,0.5)).
+invcdf_test() ->
+        ?assertEqual(0.020851637639023268, invcdf(0.1,1,5)),
+        ?assertEqual(0.010480741793785553, invcdf(0.1,1,10)),
+        ?assertEqual(0.499999999999999, invcdf(0.5,3,3)),
+        ?assertEqual(0.610181649469041, invcdf(0.7,3,3)),
+        ?assertEqual(0.9755282581475768, invcdf(0.9,0.5,0.5)).
 
-betainv_param_test() ->
-        ?assertEqual({error,"Alpha parameter =< 0 in betainv."},  betainv(0.0,-1,1)),
-        ?assertEqual({error,"Beta parameter =< 0 in betainv."},  betainv(0.0,1,-1)),
-        ?assertEqual({error,"Invalid probability"},  betainv(-0.1,1,1)).
+invcdf_param_test() ->
+        ?assertEqual({error,"Alpha parameter =< 0 in invcdf."},  invcdf(0.0,-1,1)),
+        ?assertEqual({error,"Beta parameter =< 0 in invcdf."},  invcdf(0.0,1,-1)),
+        ?assertEqual({error,"Invalid probability"},  invcdf(-0.1,1,1)).
 
 -endif.
 
