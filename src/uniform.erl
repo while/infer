67cb18e528c70591d6c6d0cea9545a2a5f62dc66
Vilhelm von Ehrenheim <vilhelm.ehrenheim@klarna.com>
2016-04-01 11:32:05 +0200

    Renamed distributions functions in uniform.

--- src/uniform.erl
+++ src/uniform.erl
@@ -3,7 +3,7 @@
 % ==============================================================================
 -module(uniform).
 
--export([unifpdf/3, unifcdf/3, unifinv/3, unifrng/3]).
+-export([pdf/3, cdf/3, invcdf/3, rnd/3]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -11,34 +11,34 @@
 
 
 % ------------------------------------------------------------------------------
-%  unifpdf - Uniform probability density function
+%  pdf - Uniform probability density function
 % ------------------------------------------------------------------------------
-unifpdf(X,A,_) when X < A -> 0.0;
-unifpdf(X,_,B) when X > B -> 0.0;
-unifpdf(_,A,B) ->
+pdf(X,A,_) when X < A -> 0.0;
+pdf(X,_,B) when X > B -> 0.0;
+pdf(_,A,B) ->
         1/(B-A).
 
 % ------------------------------------------------------------------------------
-%  unifcdf - Uniform cumulative distribution function
+%  cdf - Uniform cumulative distribution function
 % ------------------------------------------------------------------------------
-unifcdf(X,A,_) when X < A -> 0.0;
-unifcdf(X,_,B) when X > B -> 1.0;
-unifcdf(X,A,B) ->
+cdf(X,A,_) when X < A -> 0.0;
+cdf(X,_,B) when X > B -> 1.0;
+cdf(X,A,B) ->
         (X-A)/(B-A).
         
 % ------------------------------------------------------------------------------
-%  unifinv - Inverse uniform distribution function
+%  invcdf - Inverse uniform distribution function
 % ------------------------------------------------------------------------------
-unifinv(P,_,_) when P < 0 orelse P > 1 -> {error,"Invalid probability"};
-unifinv(P,A,B) ->
+invcdf(P,_,_) when P < 0 orelse P > 1 -> {error,"Invalid probability"};
+invcdf(P,A,B) ->
         A + P*(B-A).
 
 
 % ------------------------------------------------------------------------------
-%  unifrng - RNG function
+%  rnd - RNG function
 % ------------------------------------------------------------------------------
-unifrng(N,A,B) ->
-        lists:map(fun(_) -> unifinv(rand:uniform(),A,B) end, lists:seq(1,N)).
+rnd(N,A,B) ->
+        lists:map(fun(_) -> invcdf(rand:uniform(),A,B) end, lists:seq(1,N)).
 
 
 % ==============================================================================
@@ -49,51 +49,51 @@ unifrng(N,A,B) ->
 % ------------------------------------------------------------------------------
 %  pdf tests
 % ------------------------------------------------------------------------------
-unifpdf_test() ->
-        ?assertEqual(0.0, unifpdf(-0.999,0,10)),
-        ?assertEqual(0.1, unifpdf(0,0,10)),
-        ?assertEqual(0.1, unifpdf(2,0,10)),
-        ?assertEqual(0.1, unifpdf(5,0,10)),
-        ?assertEqual(0.1, unifpdf(7,0,10)),
-        ?assertEqual(0.1, unifpdf(10,0,10)),
-        ?assertEqual(0.0, unifpdf(10.001,0,10)).
+pdf_test() ->
+        ?assertEqual(0.0, pdf(-0.999,0,10)),
+        ?assertEqual(0.1, pdf(0,0,10)),
+        ?assertEqual(0.1, pdf(2,0,10)),
+        ?assertEqual(0.1, pdf(5,0,10)),
+        ?assertEqual(0.1, pdf(7,0,10)),
+        ?assertEqual(0.1, pdf(10,0,10)),
+        ?assertEqual(0.0, pdf(10.001,0,10)).
 
 
 % ------------------------------------------------------------------------------
 %  cdf tests
 % ------------------------------------------------------------------------------
-unifcdf_test() ->
-        ?assertEqual(0.0, unifcdf(-0.999,0,10)),
-        ?assertEqual(0.0, unifcdf(0,0,10)),
-        ?assertEqual(0.2, unifcdf(2,0,10)),
-        ?assertEqual(0.5, unifcdf(5,0,10)),
-        ?assertEqual(0.7, unifcdf(7,0,10)),
-        ?assertEqual(1.0, unifcdf(10,0,10)),
-        ?assertEqual(1.0, unifcdf(10.001,0,10)).
+cdf_test() ->
+        ?assertEqual(0.0, cdf(-0.999,0,10)),
+        ?assertEqual(0.0, cdf(0,0,10)),
+        ?assertEqual(0.2, cdf(2,0,10)),
+        ?assertEqual(0.5, cdf(5,0,10)),
+        ?assertEqual(0.7, cdf(7,0,10)),
+        ?assertEqual(1.0, cdf(10,0,10)),
+        ?assertEqual(1.0, cdf(10.001,0,10)).
 
 
 % ------------------------------------------------------------------------------
 %  inv tests
 % ------------------------------------------------------------------------------
-unifinv_test() ->
-        ?assertEqual(1.0, unifinv(0.1,0,10)),
-        ?assertEqual(5.0, unifinv(0.5,0,10)),
-        ?assertEqual(10.0, unifinv(1.0,0,10)).
+invcdf_test() ->
+        ?assertEqual(1.0, invcdf(0.1,0,10)),
+        ?assertEqual(5.0, invcdf(0.5,0,10)),
+        ?assertEqual(10.0, invcdf(1.0,0,10)).
 
-unifinv_error_test() ->
-        ?assertEqual({error,"Invalid probability"}, unifinv(-0.1,0,10)),
-        ?assertEqual({error,"Invalid probability"}, unifinv(1.1,0,10)).
+invcdf_error_test() ->
+        ?assertEqual({error,"Invalid probability"}, invcdf(-0.1,0,10)),
+        ?assertEqual({error,"Invalid probability"}, invcdf(1.1,0,10)).
 
 
 % ------------------------------------------------------------------------------
 %  rng tests
 % ------------------------------------------------------------------------------
-unifrng_positive_test() ->
-        [X] = unifrng(1,0,10), 
+rnd_positive_test() ->
+        [X] = rnd(1,0,10), 
         ?assert(X >= 0.0 andalso X =< 10.0).
 
-unifrng_length_test() ->
-        Xs = unifrng(23,1,3), 
+rnd_length_test() ->
+        Xs = rnd(23,1,3), 
         ?assert(length(Xs) =:= 23).
 
 
